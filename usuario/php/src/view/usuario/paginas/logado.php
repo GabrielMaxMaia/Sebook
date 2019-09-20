53ee532889a6eb9f4f3e9676a57cdb27a07f0e38
Max <49048275+MaxZumbies@users.noreply.github.com>
2019-09-19 22:34:55 -0300

    Páginas PHP sem os arquivos HTML
    Estou criando um novo branch para organizar o projeto separadamente

---
+++ usuario/php/src/view/usuario/paginas/logado.php
@@ -0,0 +1,108 @@
+<div id="container">
+		<div class="Social-Bar">
+			<a href="#">
+				<img src="images/icons/Face.png" class="icon icon-facebook" target="">
+			</a>
+			<a href="#">
+				<img src="images/icons/Instagram.png" class="icon icon-instagram" target="">
+			</a>
+			<a href="#">
+				<img src="images/icons/Twitter.png" class="icon icon-twitter" target="">
+			</a>
+		</div>
+
+		<section class="home">
+
+			<div class="banner">
+				<div id="homepage-slider" class="st-slider">
+
+					<input type="radio" class="cs_anchor radio" name="slider" id="slide1" />
+					<input type="radio" class="cs_anchor radio" name="slider" id="slide2" />
+					<input type="radio" class="cs_anchor radio" name="slider" id="slide3" />
+					<input type="radio" class="cs_anchor radio" name="slider" id="play1" checked="" />
+
+					<div class="images">
+						<div class="images-inner">
+							<div class="image-slide">
+								<div class="image">
+									<img src="images/img/banner1.jpg" alt="">
+								</div>
+							</div>
+							<div class="image-slide">
+								<div class="image">
+									<img src="images/img/banner2.jpg" alt="">
+								</div>
+							</div>
+							<div class="image-slide">
+								<div class="image">
+									<img src="images/img/banner3.jpg" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="labels">
+						<div class="fake-radio">
+							<label for="slide1" class="radio-btn"></label>
+							<label for="slide2" class="radio-btn"></label>
+							<label for="slide3" class="radio-btn"></label>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="maisProcurados">
+				<div class="container">
+					<h1>Mais Procurados</h1>
+
+					<div class="swiper-container">
+						<div class="books swiper-wrapper">
+							<div class="swiper-slide">
+								<a href="LoDescLivros.html"><img src="images/img/livroHarry.jpg" alt="livroHarry" title="Livro"></a>
+							</div>
+							<div class="swiper-slide">
+								<a href="LoDescLivros2.html"><img src="images/img/livroNeve.jpg" alt="livroNeve" title="Livro"></a>
+							</div>
+							<div class="swiper-slide">
+								<a href="LoDescLivros3.html"><img src="images/img/Capa_Como-eu-era-antes-de-voce2.jpg" alt="livroPercy" title="Livro"></a>
+							</div>
+							<div class="swiper-slide">
+								<a href="LoDescLivros4.html"><img src="images/img/Cidades-de-Papel.jpg" alt="livroHarry" title="Livro"></a>
+							</div>
+							<div class="swiper-slide">
+								<a href="LoDescLivros5.html"><img src="images/img/DepoisDeVoce_Jojo.jpg" alt="livroNeve" title="Livro"></a>
+							</div>
+							<div class="swiper-slide">
+								<a href="LoDescLivros6.html"><img src="images/img/livroPercy.jpg" alt="livroPercy" title="Livro"></a>
+							</div>
+							<div class="swiper-slide">
+								<a href="LoDescLivros7.html"><img src="images/img/Livro-A-Culpa-e-das-Estrelas.jpg" alt="livroHarry" title="Livro"></a>
+							</div>
+							<div class="swiper-slide">
+								<a href="LoDescLivros8.html"><img src="images/img/Livro-After.jpg" alt="livroNeve" title="Livro"></a>
+							</div>
+							<div class="swiper-slide">
+								<a href="LoDescLivros9.html"><img src="images/img/Livro-As-Cronicas-de-Gelo-e-Fogo-A-Danca-dos-Dragoes.jpg" alt="livroPercy" title="Livro"></a>
+							</div>
+						</div>
+						<!-- Add Pagination -->
+						<div class="swiper-pagination"></div>
+					</div>
+				</div>
+			</div>
+			<div class="eventos">
+				<div class="container">
+					<h2>Eventos</h2>
+					<figure>
+						<img src="images/img/banner2.jpg" alt="">
+						<figcaption>Festa do Livro da USP</figcaption>
+					</figure>
+					<figure>
+						<img src="images/img/banner3.jpg" alt="">
+						<figcaption>Feira Nacional Do Livro</figcaption>
+					</figure>
+				</div>
+			</div>
+		</section>
+	</div>
+	</div>
---
+++ usuario/php/src/view/usuario/paginas/perfil.php
@@ -0,0 +1,7 @@
+<div id="container">
+			<section class="perfil">
+	
+	<input type="file" id="upload"/>
+	<img id="im"/>
+			</section>
+	</div>
