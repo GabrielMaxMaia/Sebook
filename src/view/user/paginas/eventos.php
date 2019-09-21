b8a82b807656f63c5c472e64ea2e540b2553bb05
MaxZumbies <gabrielmax98@yahoo.com.br>
2019-09-21 17:12:36 -0300

    Organização dos arquivos

---
+++ src/view/user/paginas/eventos.php
@@ -0,0 +1,53 @@
+<div id="container">
+    <section class="eventos">
+        <div class="container">
+            <h1>Eventos</h1>
+            <figure>
+                <div class="img-eventos">
+                    <img src="images/img/banner1.jpg" alt="">
+                </div>
+                <figcaption>
+                    Festa do Livro
+                </figcaption>
+            </figure>
+            <p><strong>Local:</strong>É o mesmo das edições anteriores:
+                AV. Prof. Mello Morais, travessa C, na cidade universitária, em São Paulo.
+            </p>
+            <p>
+                <strong>Data/Hora:</strong>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                
+            </p>
+            <p>
+                <strong>Discrição:</strong> dolor sit amet consectetur adipisicing elit. 
+                Quibusdam voluptates sunt cum possimus harum officia ratione.
+                Possimus minus ab error et fuga officiis, eos, nisi expedita,
+                totam commodi doloremque vitae?
+            </p>
+
+            <hr>
+            <figure>
+                    <div class="img-eventos">
+                        <img src="images/img/banner2.jpg" alt="">
+                    </div>
+                    <figcaption>
+                        Festa do Livro
+                    </figcaption>
+                </figure>
+                <p><strong>Local:</strong>É o mesmo das edições anteriores:
+                    AV. Prof. Mello Morais, travessa C, na cidade universitária, em São Paulo.
+                </p>
+                <p>
+                    <strong>Data/Hora:</strong>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                   
+                </p>
+                <p>
+                    <strong>Discrição:</strong> dolor sit amet consectetur adipisicing elit. 
+                    Quibusdam voluptates sunt cum possimus harum officia ratione.
+                    Possimus minus ab error et fuga officiis, eos, nisi expedita,
+                    totam commodi doloremque vitae?
+                </p>
+        </div>
+    </section>
+    </div>
--- usuario/php/src/view/usuario/paginas/perfil.php
+++
@@ -1,7 +0,0 @@
-<div id="container">
-			<section class="perfil">
-	
-	<input type="file" id="upload"/>
-	<img id="im"/>
-			</section>
-	</div>
