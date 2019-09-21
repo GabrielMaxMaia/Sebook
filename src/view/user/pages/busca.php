0d87c4f99dde761ff93db82d3752aaa9c9454dd4
MaxZumbies <gabrielmax98@yahoo.com.br>
2019-09-21 18:08:24 -0300

    Linkagem das páginas

--- src/view/user/pages/busca.php
+++ src/view/user/pages/busca.php
@@ -3,38 +3,38 @@
         <div class="container">
             <div class="box-search">
                 <a href="" class="search">
-                    <img src="images/icons/search.png" alt="">
+                    <img src="<?php echo _ICONBASE_ ?>search.png" alt="">
                 </a>
                 <input type="text">
             </div>
             
             <div class="searchBooks">
                 <a href="">
-                    <img src="images/img/livroHarry.jpg" alt="">
+                    <img src="<?php echo _IMGBASE_ ?>livroHarry.jpg" alt="">
                 </a>
                 <a href="">
-                    <img src="images/img/livroHarry.jpg" alt="">
+                    <img src="<?php echo _IMGBASE_ ?>livroHarry.jpg" alt="">
                 </a>
                 <a href="">
-                    <img src="images/img/livroHarry.jpg" alt="">
+                    <img src="<?php echo _IMGBASE_ ?>livroHarry.jpg" alt="">
                 </a>
                 <a href="">
-                    <img src="images/img/livroHarry.jpg" alt="">
+                    <img src="<?php echo _IMGBASE_ ?>livroHarry.jpg" alt="">
                 </a>
                 <a href="">
-                    <img src="images/img/livroHarry.jpg" alt="">
+                    <img src="<?php echo _IMGBASE_ ?>livroHarry.jpg" alt="">
                 </a>
                 <a href="">
-                    <img src="images/img/livroHarry.jpg" alt="">
+                    <img src="<?php echo _IMGBASE_ ?>livroHarry.jpg" alt="">
                 </a>
                 <a href="">
-                    <img src="images/img/livroHarry.jpg" alt="">
+                    <img src="<?php echo _IMGBASE_ ?>livroHarry.jpg" alt="">
                 </a>
                 <a href="">
-                    <img src="images/img/livroHarry.jpg" alt="">
+                    <img src="<?php echo _IMGBASE_ ?>livroHarry.jpg" alt="">
                 </a>
                 <a href="">
-                    <img src="images/img/livroHarry.jpg" alt="">
+                    <img src="<?php echo _IMGBASE_ ?>livroHarry.jpg" alt="">
                 </a>
             </div>
         </div>
