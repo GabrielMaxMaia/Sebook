858b6496d8a500a203d2dd5477e78bc79ffa6b13
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2019-09-28 18:20:55 -0300

    Modificação de nome no index

--- src/view/user/menu/footer.php
+++ src/view/user/menu/footer.php
@@ -1,3 +1,76 @@
+<footer id="Rodape">
+    <div class="container-footer-all">
+
+        <div class="container-body">
+            <div class="coluna1">
+                <h1>Mais Informações do site</h1>
+                <p> Why do we use it?
+                    It is a long established fact that a reader will be distracted by the readable
+                    content of a page when looking at its layout. The point of using Lorem Ipsum is
+                    that it has a more-or-less normal distribution of letters, as opposed to using
+                    'Content here, content here', making it look like readable English. Many desktop
+                    publishing packages and web page editors now use Lorem Ipsum as their default
+                    model text, and a search for 'lorem ipsum' will uncover many web sites still
+                    in their infancy. Various versions have evolved over the years, sometimes by
+                    accident, sometimes on purpose (injected humour and the like).
+                </p>
+
+            </div>
+            <div class="coluna2">
+                <h1>Redes Sociais</h1>
+                <div class="row">
+                    <img src="images/icons/Face-R.png" alt="">
+                    <label>Siga-nos no Facebook</label>
+                </div>
+                <div class="row">
+                    <img src="images/icons/Twitter-R.png" alt="">
+                    <label>Siga-nos no Twitter</label>
+                </div>
+                <div class="row">
+                    <img src="images/icons/Instagram-R.png" alt="">
+                    <label>Siga-nos no Instagram</label>
+                </div>
+                <div class="row">
+                    <img src="images/icons/Google-R.png" alt="">
+                    <label>Siga-nos no Google Plus</label>
+                </div>
+            </div>
+            <div class="coluna3">
+                <h1>Informações</h1>
+                <div class="row2">
+                    <img src="images/icons/Home-R.png" alt="">
+                    <label>Why do we use it?
+                        It is a long established fact that a reader will be distracted by the readable
+                        that it has</label>
+                </div>
+                <div class="row2">
+                    <img src="images/icons/Phone-R.png" alt="Telefone">
+                    <label>+ xx xxxx-xxxx</label>
+                </div>
+                <div class="row2">
+                    <img src="images/icons/Email-R.png" alt="">
+                    <label>Fulano_de_tal@gmail.com</label>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="container-footer">
+        <div class="footer">
+            <div class="copyright">
+                @ 2019 Todos os direitos Reservados |<a href="">Sebook</a>
+            </div>
+
+            <div class="informar">
+                <a href="">Informação Companhia </a> |
+                <a href="">Privações e Politica </a> |
+                <a href="">Termos e Condições</a>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
 <footer class="master1">
     <nav class="menuMobile">
         <a href="index.html" class="home">
