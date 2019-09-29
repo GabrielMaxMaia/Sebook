55930882210081f0cdb69392c09ea1f61bab5322
MaxZumbies <gabrielmax98@yahoo.com.br>
2019-09-28 21:22:20 -0300

    Correção dos links de imagens

--- src/view/user/menuHome/livros.php
+++ src/view/user/menuHome/livros.php
@@ -3,123 +3,24 @@
         <div class="container">
             <div class="box-search">
                 <a href="" class="search">
-                    <img src="images/icons/search.png" alt="">
+                    <img src="<?php echo _ICONBASE_ ?>search.png" alt="">
                 </a>
                 <input type="text">
             </div>
+
             <div class="estante-slider">
-                <div class="titulo-estante-slider">
-                    Lançamentos
-                </div>
-                <div class="swiper-container">
-                    <div class="books swiper-wrapper">
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>Livro-As-Cronicas-de-Gelo-e-Fogo.jpg" alt="livroHarry" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>Livro-Cinquenta-Tons-de-Cinza.jpg" alt="livroNeve" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>Livro-Jogos-Vorazes.jpg" alt="livroPercy" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>Livro-O-Diario-de-Anne-Frank.jpg" alt="livroHarry" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>Livro-O-Lado-Bom-da-Vida.jpg" alt="livroNeve" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>livro-belo-disastre.jpg" alt="livroPercy" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>livro-maus.jpg" alt="livroHarry" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>livro-mister.jpg" alt="livroNeve" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>livro-quem-e-voce-alasca.jpg" alt="livroPercy" title="Livro"></a>
-                        </div>
-                    </div>
-                    <!-- Add Pagination -->
-                    <div class="swiper-pagination"></div>
-                </div>
+                <div class="titulo-estante-slider">Mais procurados</div>
+                <?php require('src/view/user/util/slick.php');?>
             </div>
+
             <div class="estante-slider">
-                <div class="titulo-estante-slider">
-                    Mais Vendidos 
-                </div>
-                <div class="swiper-container">
-                    <div class="books swiper-wrapper">
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>miseraveis.jpg" alt="livroHarry" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>o-morro-dos-ventos-uivantes.jpg" alt="livroNeve" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>outlander.jpg" alt="livroPercy" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>livro-maus.jpg" alt="livroHarry" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>Livro-After.jpg" alt="livroNeve" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>DepoisDeVoce_Jojo.jpg" alt="livroPercy" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>Livro-Cinquenta-Tons-de-Cinza.jpg" alt="livroHarry" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>livroNeve.jpg" alt="livroNeve" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>livroPercy.jpg" alt="livroPercy" title="Livro"></a>
-                        </div>
-                    </div>
-                    <!-- Add Pagination -->
-                    <div class="swiper-pagination"></div>
-                </div>
+                <div class="titulo-estante-slider">Lançamentos</div>
+                <?php require('src/view/user/util/slick.php');?>
             </div>
+
             <div class="estante-slider">
-                <div class="titulo-estante-slider">
-                    Romance
-                </div>
-                <div class="swiper-container">
-                    <div class="books swiper-wrapper">
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>Livro-Cinquenta-Tons-de-Cinza.jpg" alt="livroHarry" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>livro-mister.jpg" alt="livroNeve" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>livro-quem-e-voce-alasca.jpg" alt="livroPercy" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>livro-belo-disastre.jpg" alt="livroHarry" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros3.html"><img src="<?php echo _IMGBASE_ ?>Capa_Como-eu-era-antes-de-voce2.jpg" alt="livroNeve" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>Livro-After.jpg" alt="livroPercy" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros4.html"><img src="<?php echo _IMGBASE_ ?>Cidades-de-Papel.jpg" alt="livroHarry" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros2.html"><img src="<?php echo _IMGBASE_ ?>o-morro-dos-ventos-uivantes.jpg" alt="livroNeve" title="Livro"></a>
-                        </div>
-                        <div class="swiper-slide">
-                            <a href="desLivros.html"><img src="<?php echo _IMGBASE_ ?>outlander.jpg" alt="livroPercy" title="Livro"></a>
-                        </div>
-                    </div>
-                    <!-- Add Pagination -->
-                    <div class="swiper-pagination"></div>
-                </div>
+                <div class="titulo-estante-slider">Recomendados</div>
+                <?php require('src/view/user/util/slick.php');?>
             </div>
         </div>
     </section>
