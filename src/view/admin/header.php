346279be0b27fb25c841d8f5039995e1d614d22b
Max <49048275+MaxZumbies@users.noreply.github.com>
2019-10-02 20:53:12 -0300

    Atualização da pasta "admin"

---
+++ src/view/admin/header.php
@@ -0,0 +1,8 @@
+<div class="menuGroup">
+    <a class="logo" href="indexM.html">
+        <img src="<?php echo _IMGBASE_ ?>logoSebookCor.png" alt="SebooK">
+    </a>
+    <a class="perfil" href="login.html">
+        <img src="<?php echo _ICONBASE_ ?>Logado.png" alt="Perfil" title="Perfil">
+    </a>
+</div>
---
+++ src/view/admin/pages/aprovarSebo.php
@@ -0,0 +1,41 @@
+<div id="container">
+    <div class="aprovaSebo">
+        <li>
+            <a href="#">
+                <table border="1">
+                    <tr>
+                        <th>Nome Sebo</th>
+                        <th>Data do Cadastro</th>
+                        <th>Atendeu todos os requisitos? S/N</th>
+                        <th><p>Apovado? : S/N:</p></th>
+                        <th>Obervações</th>
+                    </tr>
+                    <tr>
+                        <td>Sebo do Lobão</td>
+                        <td>01/08/2019</td>
+                        <td>
+                            <label for="sim"><input name="atendeu?" id="sim" value="S" type="radio">SIM</label>
+                            <label for="nao"><input name="atendeu?" id="nao" value="N" type="radio">NAO</label>
+                        </td>
+                        <td>
+                            <label for="sim"><input name="aprovado ou não" id="sim" value="S" type="radio">SIM</label>
+                            <label for="nao"><input name="aprovado ou não" id="nao" value="N" type="radio">NAO</label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Ana Sebo</td>
+                        <td>03/08/2019</td>
+                        <td>
+                            <label for="sim"><input name="atendeu?" id="sim" value="S" type="radio">SIM</label>
+                            <label for="nao"><input name="atendeu?" id="nao" value="N" type="radio">NAO</label>
+                        </td>
+                        <td>
+                            <label for="sim"><input name="aprovado ou não" id="sim" value="S" type="radio">SIM</label>
+                            <label for="nao"><input name="aprovado ou não" id="nao" value="N" type="radio">NAO</label>
+                        </td>
+                    </tr>
+                </table>
+            </a>
+        </li>
+    </div>
+</div>
