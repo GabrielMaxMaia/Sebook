56ad4ce1f3329599e8aa66ba86c2769f431784c9
GABRIEL MAX MAIA DO CARMO <gabriel.mmcarmo@senacsp.edu.br>
2019-10-25 14:54:43 -0300

    Comentario dentro do config.php

--- config/config.php
+++ config/config.php
@@ -2,18 +2,20 @@
 
 require_once $ajuste.'src/util/Psr4AutoloaderClass.php';
 
+// Todos os links estão usando apenas a _URLBASE_ como constante
+
 define("_URLBASE_","http://localhost/sebook/");
 
-define("_URLBASEMENU_","http://localhost/sebook/index.php?area=user");
-define("_URLBASEADM_","http://localhost/sebook/index.php?area=adm");
+// define("_URLBASEMENU_","http://localhost/sebook/index.php?area=user");
+// define("_URLBASEADM_","http://localhost/sebook/index.php?area=adm");
 
 //http://localhost/sebook/index.php?area=user&pasta=menuHome&page=sebos
 
 //Constantes para importar IMG, CSS e ICONS
-define("_CSSBASEUSER_","http://localhost/sebook/public/css/usuario.css");
-define("_CSSBASEADM_","http://localhost/sebook/public/css/adm.css");
-define("_IMGBASE_","http://localhost/sebook/public/img/");
-define("_ICONBASE_","http://localhost/sebook/public/icon/");
+// define("_CSSBASEUSER_","http://localhost/sebook/public/css/usuario.css");
+// define("_CSSBASEADM_","http://localhost/sebook/public/css/adm.css");
+// define("_IMGBASE_","http://localhost/sebook/public/img/");
+// define("_ICONBASE_","http://localhost/sebook/public/icon/");
 
 date_default_timezone_set('America/Sao_Paulo'); 
 
