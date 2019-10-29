771c2086831cd592e5aeef60d573ab4ba5c67049
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2019-10-29 18:38:39 -0300

    db

--- config/script_bd/db_sebook.sql
+++ config/script_bd/db_sebook.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 29-Out-2019 às 05:18
+-- Generation Time: 29-Out-2019 às 22:36
 -- Versão do servidor: 10.1.38-MariaDB
 -- versão do PHP: 7.3.2
 
