4b1c1eb018dbd27562e4545a721988b07b7b5d8c
Max <49048275+MaxZumbies@users.noreply.github.com>
2019-11-07 22:20:56 -0300

    Atualização dos proj. de mapas | Marcos NÃO apagar

---
+++ src/view/user/geoLocalizacao/mapa.php
@@ -0,0 +1,86 @@
+<head>
+    <script type='text/javascript' charset='utf-8' src='https://js.api.here.com/v3/3.1/mapsjs-core.js'></script>
+    <script type='text/javascript' charset='utf-8' src='https://js.api.here.com/v3/3.1/mapsjs-core-legacy.js'></script>
+    <script type='text/javascript' charset='utf-8' src='https://js.api.here.com/v3/3.1/mapsjs-service.js'></script>
+    <script type='text/javascript' charset='utf-8' src='https://js.api.here.com/v3/3.1/mapsjs-service-legacy.js'></script>
+    <script type='text/javascript' charset='utf-8' src='https://js.api.here.com/v3/3.1/mapsjs-ui.js'></script>
+    <script type='text/javascript' charset='utf-8' src='https://js.api.here.com/v3/3.1/mapsjs-mapevents.js'></script>
+    <link rel='stylesheet' type='text/css' href='https://js.api.here.com/v3/3.1/mapsjs-ui.css'>
+<head>
+    
+<body>    
+    <form class="search-box" name="form_pesquisa" id="form_pesquisa" method="post" action="">
+        <div class="input-prepend">
+            <span class="add-on"><i class="icon-search"></i></span>
+            <input type="text" name="pesquisaLivro" id="pesquisaLivro" value="" tabindex="1" placeholder="Pesquisar Livros..." />
+        </div>
+    </form>
+
+    <div id="msg"></div>
+	<div style="width: 100%; height: 480px; border: solid gainsboro;" id="mapContainer"></div>
+	<script>
+		// Instantiate a map and platform object:
+		var platform = new H.service.Platform({
+			'apikey': 'pNTev5pwXgLcdHoNuFVh3YJmNk9Kr0VB6mdPBvMzVec'
+		});
+		// Retrieve the target element for the map:
+		var targetElement = document.getElementById('mapContainer');
+
+		// Get default map types from the platform object:
+		var defaultLayers = platform.createDefaultLayers();
+
+		// Instantiate the map:
+		var map = new H.Map(document.getElementById('mapContainer'),
+			defaultLayers.vector.normal.map,{
+			center: {lat:-23.506363, lng:-46.882215},
+			zoom: 12,
+			pixelRatio: window.devicePixelRatio || 1
+		});
+
+		//Criando Marcadores
+		var itbEngenho = new H.map.Marker({lat:-23.487558, lng:-46.888370});
+        itbEngenho.setData("Engenho");
+		map.addObject(itbEngenho);
+
+		var itbBelval= new H.map.Marker({lat:-23.511261, lng:-46.889910});
+        itbBelval.setData("Belval");
+		map.addObject(itbBelval);
+
+		var itbViana= new H.map.Marker({lat:-23.546118, lng:-46.869870 });
+        itbViana.setData("Viana");
+		map.addObject(itbViana);
+
+        var mac = new H.map.Marker({lat:-23.492516, lng:  -46.881449 });
+        mac.setData("Mac..");
+		map.addObject(mac);
+		// Add behavior to the map: panning, zooming, dragging.
+		var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+		// Create the default UI:
+		var ui = H.ui.UI.createDefault(map, defaultLayers, 'pt-BR');
+
+        findNearestMarker();
+
+        function findNearestMarker() {
+            
+            var minDist = 5000,
+                nearest_text = '*None*',
+                markerDist,
+                // get all objects added to the map
+                objects = map.getObjects(),
+                len = map.getObjects().length,
+                i;
+
+            // iterate over objects and calculate distance between them
+            for (i = 0; i < len; i += 1) {
+                markerDist = objects[i].getGeometry().distance({lat:-23.492516, lng:  -46.881449 });
+                console.log(markerDist + " < " + minDist +" marcador: "+ objects[i].getData() );
+                console.log(markerDist < minDist);
+                if (markerDist > 0  && markerDist < minDist) {
+                    minDist = markerDist;
+                    nearest_text = objects[i].getData();
+                }
+            }
+            document.getElementById('msg').innerHTML = 'The nearest marker is: ' + nearest_text;
+        }
+	</script>
