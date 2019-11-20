d07002798c12b0cbc671211c3d41ae5ac295d921
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2019-11-20 01:25:49 -0300

    Alterações de banners

--- src/view/user/util/banner.php
+++ src/view/user/util/banner.php
@@ -2,25 +2,27 @@
     <div id="homepage-slider" class="st-slider">
 
         <input type="radio" class="cs_anchor radio" name="slider" id="slide1" />
+        <!-- <input type="radio" class="cs_anchor radio" name="slider" id="slide2" /> -->
         <input type="radio" class="cs_anchor radio" name="slider" id="slide2" />
-        <input type="radio" class="cs_anchor radio" name="slider" id="slide3" />
+
         <input type="radio" class="cs_anchor radio" name="slider" id="play1" checked="" />
+        
 
         <div class="images">
             <div class="images-inner">
                 <div class="image-slide">
                     <div class="image">
-                    <a href="<?=_URLBASE_?>area/user/pages/artesoesDoacao"><img src="<?= _URLBASE_ ?>public/img/imgBanner/banner1.jpg" alt=""></a>
+                    <a href="<?=_URLBASE_?>area/user/pages/artesoesDoacao"><img src="<?= _URLBASE_ ?>public/img/imgBanner/doacaoBanner.png" alt=""></a>
                     </div>
                 </div>
-                <div class="image-slide">
+                <!-- <div class="image-slide">
                     <div class="image">
-                        <img src="<?= _URLBASE_ ?>public/img/imgBanner/banner2.jpg" alt="">
+                        <img src="<?//= _URLBASE_ ?>public/img/imgBanner/banner2.jpg" alt="">
                     </div>
-                </div>
+                </div> -->
                 <div class="image-slide">
                     <div class="image">
-                    <a href="<?=_URLBASE_?>area/user/pages/artesoes"><img src="<?= _URLBASE_ ?>public/img/imgBanner/banner3.jpg" alt=""></a>
+                    <a href="<?=_URLBASE_?>area/user/pages/artesoes"><img src="<?= _URLBASE_ ?>public/img/imgBanner/artesaoBanner.png" alt=""></a>
                     </div>
                 </div>
             </div>
@@ -29,8 +31,8 @@
         <div class="labels">
             <div class="fake-radio">
                 <label for="slide1" class="radio-btn"></label>
+                <!-- <label for="slide2" class="radio-btn"></label> -->
                 <label for="slide2" class="radio-btn"></label>
-                <label for="slide3" class="radio-btn"></label>
             </div>
         </div>
     </div>
